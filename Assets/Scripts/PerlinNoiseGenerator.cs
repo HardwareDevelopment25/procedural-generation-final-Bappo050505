efd52c24fdf805e1bca606ec62f37138f33cae01
Bappo050505 <jakubson050505@gmail.com>
2025-11-05 16:54:08 +0000

    made a perlin noise map however the colouring is currently broken.

---
+++ Assets/Scripts/PerlinNoiseGenerator.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public static class PerlinNoiseGenerator
+{
+
+    public static float[,] GenerateFalloffMap(int size, AnimationCurve curve)
+    {
+        float[,] falloffMap = new float[size, size];
+
+        for (int i = 0; i < size; i++) 
+        { 
+            for (int j = 0; j < size; j++)
+            {
+                float x = i / (float)size * 2 - 1;
+                float y = j / (float)size * 2 - 1;
+
+                float value = Mathf.Max(Mathf.Abs(x) , Mathf.Abs(y));
+
+                falloffMap[i, j] = curve.Evaluate(value);
+            }
+        }
+
+        
+
+        return falloffMap;
+    }
+
+    public static float[,] GenerateNoiseMap (int mapHeight, int mapWidth ,int octave,  float scale , float lacunarity , float persistance , int seed , Vector2 offset)
+    {
+        float[,] noiseMap = new float[mapHeight, mapWidth];
+        if (scale == 0) scale = 0.0001f;
+
+        float maxPossibleHeight = float.MinValue;
+        float minPossibleHeight = float.MaxValue;
+
+        System.Random rand = new System.Random(seed);
+
+        Vector2[] octaveOffset = new Vector2[octave]; // try have less than 5 offset
+
+
+
+            //creates x amount of samples of offsets for variation and randomness
+            for (int i = 0; i < octave; i++)
+            {
+                float offsetx = rand.Next(-100000, 100000) + offset.x;
+                float offsety = rand.Next(-100000, 100000) + offset.y;
+
+                octaveOffset[i] = new Vector2 (offsetx, offsety);
+            }
+            //creates the perlin map
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for(int x =0; x< mapWidth; x++)
+                {
+                    float amplitude = 1, frequencey = 1, noiseheight = 1;
+
+                    for (int i = 0; i < octave; i++) //creates individual perlin noise maps
+                    {
+
+                        float sampleX = ((float)(x - mapWidth / 2)) / scale * frequencey + octaveOffset[i].x;
+                        float sampleY = ((float)(y - mapHeight / 2)) / scale * frequencey + octaveOffset[i].y;
+
+                        float perlinResult = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                        noiseheight += perlinResult * amplitude;
+                        amplitude *= persistance;
+                        frequencey *= lacunarity;
+
+                    }
+                    //we are after the highest peak and the lowest peak for our future lerp
+                    if (noiseheight > maxPossibleHeight) maxPossibleHeight = noiseheight;
+                    else if (noiseheight < minPossibleHeight) minPossibleHeight = noiseheight;
+
+                    noiseMap[x, y] = Mathf.InverseLerp(minPossibleHeight, maxPossibleHeight, noiseheight);
+
+
+
+                }
+
+            }
+        
+
+        return noiseMap;
+    }
+
+
+
+}
---
+++ Assets/Scripts/TextureLogicalAnd.cs
@@ -0,0 +1,151 @@
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class TextureLogicalAnd : MonoBehaviour
+{
+    public enum DrawMode {noisemap, colourmap }
+    public DrawMode drawMode = DrawMode.noisemap;
+
+    [System.Serializable]
+    public struct TerrainHeights
+    {
+        public string name;
+        public float height;
+        public Color color;
+    }
+
+    public TerrainHeights[] terrain;
+
+
+    float currentHeight;
+    public int ImageSize = 64;
+    public int seed = 0;
+    public float scale = 10f;
+    public int octave = 5;
+    public float lacunarity = 2;
+    public float persistance = 1;
+    public Vector2 offset;
+
+    public AnimationCurve curve;
+
+    Color[,] colourmap;
+
+    private Color[] pix;
+    Texture2D texture;
+    System.Random random;
+
+    
+
+    private void Start()
+    {
+        
+
+        random = new System.Random();
+        texture = new Texture2D(ImageSize, ImageSize);
+        pix = new Color[texture.width * texture.height];
+        //createPattern();
+        //createNoiseMap();
+        generatePattern();
+        
+        GetComponent<MeshRenderer>().material.mainTexture = texture;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            generatePattern();
+        }
+    }
+
+    //creating a fractal
+    public void createPattern()
+    {
+        //go through each pixle in each texture
+
+        for(int y = 0; y < texture.height; y++)
+        {
+            for (int x = 0; x < texture.width; x++)
+            {
+                Color pixleColour = ((x & y) !=0?Color.white:Color.black);
+                texture.SetPixel(x, y, pixleColour);
+            }
+        }
+        texture.Apply();
+    }
+
+    //creating a noise map
+    public void createNoiseMap()
+    {
+        if (scale == 0) scale = 0.00001f;
+
+        for (int y = 0; y < texture.height; y++)
+        {
+            for (int x = 0; x < texture.width; x++)
+            {
+                float xCoord = (float)x/texture.width * scale;
+                float ycoord = (float)y/texture.height * scale;
+                float Noise = Mathf.PerlinNoise(xCoord, ycoord);
+                pix[(int)y * texture.width + (int)x] = new Color(Noise,Noise,Noise);
+
+                
+            }
+        }
+        texture.SetPixels(pix);
+        texture.Apply();
+    }
+
+    //generating a multi layered perlin noise map
+    public void generatePattern()
+    {
+        float[,] noisemap = PerlinNoiseGenerator.GenerateNoiseMap(ImageSize, ImageSize, octave, scale, lacunarity, persistance, random.Next(), offset);
+
+        for (int i = 0; i < noisemap.GetLength(0);  i++)
+        {
+            for(int j = 0; j < noisemap.GetLength(1); j++)
+            {
+                texture.SetPixel(i, j, new Color(noisemap[i, j], noisemap[i, j], noisemap[i, j]));
+            }
+        }
+        texture.Apply();
+
+        Colour(noisemap);
+    }
+
+    public void Colour(float[,] noisemap)
+    {
+
+        
+        for(int i = 0; i < texture.width; i++)
+        {
+            for (int j = 0 ; j < texture.height; j++)
+            {
+                currentHeight = noisemap[i, j];
+
+                for(int k = 0; k < terrain.Length; k++)
+                {
+                    if(currentHeight <= terrain[k].height)
+                    {
+                        colourmap[i,j] = terrain[k].color;
+                        break;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < colourmap.GetLength(0); i++)
+        {
+            for (int j = 0; j < colourmap.GetLength(1); j++)
+            {
+                texture.SetPixel(i, j, colourmap[i, j]);
+            }
+        }
+        texture.Apply();
+        drawMode = DrawMode.colourmap;
+    }   
+
+
+    
+   
+}
